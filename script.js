4c608add4f801ac4115a3f020c89e8e7f480c281
TMKLYugi <khangpython@outlook.com.vn>
2021-04-05 21:49:31 +0700

    05-04-2021 21:49

---
+++ script.js
@@ -0,0 +1,10 @@
+let dev = {
+    open: function() {
+        document.write('Xin chào Nhà sáng tạo!')
+        console.log('Xin chào Khang - Nhà phát triển')
+        }
+   }
+   const num = 100;
+   if (num > 10){
+     console.log('Ê')
+   }
